df1a136e5b6e413726c8e76186a771bce5af2abc
SAMUEL ALAN GENTIL LOPES <samuel.aglopes@senacsp.edu.br>
2025-10-22 22:12:30 -0300

    projeto variáveis

---
+++ aulas/Program.cs
@@ -0,0 +1,16 @@
+﻿// comentário
+// Console.WriteLine("Qualquer coisa");
+ 
+// 1. VARIÁVEIS E TIPOS
+ 
+var n = 10;
+int x = 1;
+double preco = 10.50;
+char inicial = 'M';
+string nome = "Matheus";
+bool status = true;
+int idade = 31;
+ 
+Console.WriteLine($"Status: {status}");
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Idade: {idade}");
